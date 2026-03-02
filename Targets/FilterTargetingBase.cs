f13705ddeedc983f330d1e49d3d5be2854cf06d6
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-02 03:08:12 +0300

    Add FilterTargetingBase

---
+++ Targets/FilterTargetingBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOBetterIntentsAndRewording.Targets
+{
+    public abstract class FilterTargetingBase : BaseCombatTargettingSO
+    {
+        public BaseCombatTargettingSO orig;
+
+        public override bool AreTargetAllies => orig.AreTargetAllies;
+        public override bool AreTargetSlots => orig.AreTargetSlots;
+
+        public override TargetSlotInfo[] GetTargets(SlotsCombat slots, int casterSlotID, bool isCasterCharacter)
+        {
+            var origTargets = orig.GetTargets(slots, casterSlotID, isCasterCharacter);
+            var targets = new List<TargetSlotInfo>();
+
+            foreach (var target in origTargets)
+            {
+                if (!ShouldIncludeTarget(target, slots, casterSlotID, isCasterCharacter))
+                    continue;
+
+                targets.Add(target);
+            }
+
+            return [..targets];
+        }
+
+        public abstract bool ShouldIncludeTarget(TargetSlotInfo target, SlotsCombat slots, int casterSlotID, bool isCasterCharacter);
+    }
+}
