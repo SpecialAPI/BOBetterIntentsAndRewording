59f96babfb80fdf23e6611d51c216725a799aa8c
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-02 03:16:44 +0300

    Update intents for the Synod's Exhume ability

--- Changes/SynodChanges.cs
+++ Changes/SynodChanges.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BOBetterIntentsAndRewording.Tools;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -11,7 +12,8 @@ namespace BOBetterIntentsAndRewording.Changes
             var exhume = LoadedAssetsHandler.GetEnemyAbility("Exhume_A");
             exhume.intents = new()
             {
-                TargetIntent(Targeting.Slot_SelfSlot, IntentType_GameIDs.Other_Spawn.ToString(), IntentType_GameIDs.Other_Spawn.ToString(), IntentType_GameIDs.Other_Spawn.ToString(), IntentType_GameIDs.Other_Spawn.ToString())
+                TargetIntent(Targeting.Slot_AllyAllSlots.FilterByOccupied(false), IntentType_GameIDs.Misc_Hidden.ToString()),
+                TargetIntent(Targeting.Slot_SelfSlot, IntentType_GameIDs.Status_Scars.ToString(), IntentType_GameIDs.Other_Spawn.ToString(), IntentType_GameIDs.Other_Spawn.ToString(), IntentType_GameIDs.Other_Spawn.ToString(), IntentType_GameIDs.Other_Spawn.ToString())
             };
         }
     }
