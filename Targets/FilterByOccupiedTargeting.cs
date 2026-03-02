260103adaca60ae738ba4f7cfb1a6ce88de2a6cf
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-02 03:10:54 +0300

    Add FilterByOccupiedTargeting

---
+++ Targets/FilterByOccupiedTargeting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOBetterIntentsAndRewording.Targets
+{
+    public class FilterByOccupiedTargeting : FilterTargetingBase
+    {
+        public bool targetsShouldBeOccupied;
+
+        public override bool ShouldIncludeTarget(TargetSlotInfo target, SlotsCombat slots, int casterSlotID, bool isCasterCharacter)
+        {
+            return target.HasUnit == targetsShouldBeOccupied;
+        }
+    }
+}
---
+++ Tools/LocalTargetingTools.cs
@@ -0,0 +1,20 @@
+﻿using BOBetterIntentsAndRewording.Targets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace BOBetterIntentsAndRewording.Tools
+{
+    public static class LocalTargetingTools
+    {
+        public static BaseCombatTargettingSO FilterByOccupied(this BaseCombatTargettingSO orig, bool targetsShouldBeOccupied)
+        {
+            var t = ScriptableObject.CreateInstance<FilterByOccupiedTargeting>();
+            t.orig = orig;
+            t.targetsShouldBeOccupied = targetsShouldBeOccupied;
+
+            return t;
+        }
+    }
+}
